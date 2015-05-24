b438772833c4d3b5ed6a1fa22161c9e9afa745a2
namilica <namilica@users.noreply.github.com>
2015-05-24 21:48:33 +0900

    reimplement autoloader

--- lib/autoload.php
+++ lib/autoload.php
@@ -2,13 +2,14 @@
 
 spl_autoload_register(function ($class){
 	$name = explode('\\', $class.'.php');
-	$file = LIB_DIR.implode(DIRECTORY_SEPARATOR, $name);
-	unset($name[0]);
-	$app = APP_DIR.implode(DIRECTORY_SEPARATOR, $name);
+	if(reset($name)=='app'){
+		unset($name[0]);
+		$file = APP_DIR.implode(DIRECTORY_SEPARATOR, $name);
+	}else{
+		$file = LIB_DIR.implode(DIRECTORY_SEPARATOR, $name);
+	}
 	if(file_exists($file))
 		include($file);
-	else if(file_exists($app))
-		include($app);
 	else
 		throw new Exception("Files for class $class not found in $file or $app");
 });
